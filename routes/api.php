be73d69f366038d9a75aa0b727b52da3b9aba31c
Tristan Bendixen <angelod1981@gmail.com>
2018-03-16 15:22:58 +0100

    Added sensor data to a random file for future work because why the heck not?

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,16 @@
 use Illuminate\Http\Request;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+| Temperature: -40 to 125 (Acc: 0.015)
+| Movement: Boolean
+| Gas - CO2: 400ppm to 8192ppm (clipped)
+| Gas - TVOC: 0ppb 1187ppb (clipped)
+| Humidity: 0 to 100 %RH (Acc: 0.008)
+| Pressure:
+|   Absolute: 300 hPa to 1100 hPa (Acc: 1.0 at 0 to 65 °C)
+|   Relative: 700 hPa to 900 hPa (Acc: 0.12 at 24 to 40 °C)
+| UV: ???
+| Ambient Light: ??? (Lux?)
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
